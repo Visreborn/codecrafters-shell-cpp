2ac67a4410c3c0e3df3766bb3deee949e437a652
Truong Vinh <cheemlovescakes@gmail.com>
2026-03-15 23:13:13 +0700

    Updated the code with the print function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<utility>
 
 using std :: cin;
 using std :: cout;
@@ -8,24 +9,44 @@ using std :: cerr;
 using std :: string;
 using std :: endl;
 
-bool status(string input) {
+int skip_ws(int i, const string &input) { // skip white spaces
+	while(i < input.size() && input[i] == ' ') {
+		i ++;
+	}
+
+	return i;
+}
+
+std :: pair<int, string> get_token(int i, const string &input) { // get the token and return the final index after getting it
+	string tmp;
+
+	while(i < input.size() && input[i] != ' ') {
+		tmp += input[i];
+		i ++;
+	}
+
+	return {i, tmp};
+}
+
+bool status(const string &input) {
 	std :: vector<string> tokens;
 
 	int i = 0; 
 
-	while(i < input.size()) {
-		string tmp;
+	while(1) {
+		// skip all whitespaces
+		i = skip_ws(i, input);
 
-		while(i < input.size() && input[i] != ' ') {
-			tmp += input[i];
-			i ++;
+		if(i == input.size()) {
+			break;
 		}
 
-		if(tmp.size() > 0) {
-			tokens.push_back(tmp);
-		}
+		std :: pair<int, string> ans = get_token(i, input);
+		i = ans.first;
 
-		i ++;
+		if(ans.second.size() > 0) {
+			tokens.push_back(ans.second);
+		}
 	}
 
 	if(tokens[0] == "exit" && tokens.size() == 1) {
@@ -33,6 +54,49 @@ bool status(string input) {
 	} else {
 		return 1;
 	}
+}
+
+void print(int i, const string &input) {
+    std::vector<string> tokens;
+
+    while(i < input.size()) { 
+
+		i = skip_ws(i, input);
+        if(i == input.size()) break;
+
+        std::pair<int, string> ans = get_token(i, input);
+        tokens.push_back(ans.second);
+        
+        i = ans.first;
+    }
+
+    for(int j = 0; j < tokens.size(); j++) {
+        cout << tokens[j] << ' ';
+    }
+
+    cout << endl;
+}
+
+void exe(const string &input) {
+	if(status(input) == 0) {
+		exit(0);
+	}
+
+	string first_token;
+
+	int i = 0;
+	
+	i = skip_ws(i, input);
+	std :: pair<int, string> ans = get_token(i, input);
+	i = ans.first;
+
+	// doing the corresponded function
+	if(ans.second == "echo") {
+		print(i, input);
+		return;
+	}
+
+	cout << input << ": command not found" << endl;
 }
 
 int main() {
@@ -47,10 +111,6 @@ int main() {
 		string input;
 		getline(std :: cin, input);
 
-		if(status(input) == 0) {
-			return 0;
-		}
-
-		cout << input << ": command not found" << endl;
+		exe(input);
 	}
 }
