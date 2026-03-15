ef8951d48122dca83fa50830e6d322b4ea126e0e
Truong Vinh <cheemlovescakes@gmail.com>
2026-03-15 22:32:57 +0700

    Done with being able to exit a shell

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std :: cin;
 using std :: cout;
 using std :: cerr;
 using std :: string;
 using std :: endl;
+
+bool status(string input) {
+	std :: vector<string> tokens;
+
+	int i = 0; 
+
+	while(i < input.size()) {
+		string tmp;
+
+		while(i < input.size() && input[i] != ' ') {
+			tmp += input[i];
+			i ++;
+		}
+
+		if(tmp.size() > 0) {
+			tokens.push_back(tmp);
+		}
+
+		i ++;
+	}
+
+	if(tokens[0] == "exit" && tokens.size() == 1) {
+		return 0;
+	} else {
+		return 1;
+	}
+}
 
 int main() {
 	// Flush after every std::cout / std:cerr
@@ -18,6 +46,10 @@ int main() {
 
 		string input;
 		getline(std :: cin, input);
+
+		if(status(input) == 0) {
+			return 0;
+		}
 
 		cout << input << ": command not found" << endl;
 	}
